116d7615ac1997505bd15de7c817c926d5c01eed
Aloysius <a.chia@digipen.edu>
2024-01-05 19:29:49 +0800

    cahnged to traccar

---
+++ AndroidApp/app/build/generated/source/buildConfig/androidTest/regular/debug/org/traccar/client/test/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.traccar.client.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.traccar.client.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "regular";
+}
